33ae3be241fa62c74e53546513fa05dd7dea4e3b
maxi falcone <maxifalcone@gmail.com>
2019-03-28 17:55:10 -0300

    mas cambios

--- wp-content/themes/festivalpensamientocontemporaneo/footer.php
+++ wp-content/themes/festivalpensamientocontemporaneo/footer.php
@@ -6,13 +6,33 @@
 
 <footer class="site-footer pt-5 pb-5">
 	<div class="container">
+		<div class="row main-logos">
+			<div class="col-md-6">
+				<div class="text-center">
+					<a href="https://www.santafe.gov.ar/" target="_blank">
+						<img src="<?php bloginfo('template_directory'); ?>/img/logostafe.png" alt="Gobierno de Santa Fe" class="gobierno">
+					</a>
+			</div>
+			</div>
+			<div class="col-md-6">
+				<div class="text-center">
+					<a href="http://cfi.org.ar" target="_blank">
+						<img src="<?php bloginfo('template_directory'); ?>/img/logocfi.png" alt="Consejo Federal de Inversiones" class="cfi">
+					</a>
+				</div>
+			</div>
+		</div>
 		<div class="row">
-			<div class="col-md-2 offset-md-3">
-				<a href="https://www.santafe.gov.ar/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/logostafe.png" alt="Gobierno de Santa Fe" class="gobierno"></a>
-			</div>
-			<div class="col-md-4">
-				<a href="http://cfi.org.ar" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/logocfi.png" alt="Consejo Federal de Inversiones" class="cfi"></a>
-			</div>
+				
+				<a href="http://revistaanfibia.com" target="_blank">
+					<img src="<?php bloginfo('template_directory'); ?>/img/anfibialogo.png" alt="Anfibia" class="cfi">
+				</a>
+				<a href="http://www.unsam.edu.ar/" target="_blank">
+					<img src="<?php bloginfo('template_directory'); ?>/img/logousam.png" alt="UNSAM" class="cfi">
+				</a>
+				<a href="http://cronoslab.org/" target="_blank">
+					<img src="<?php bloginfo('template_directory'); ?>/img/logocronos.png" alt="Cronos" class="cfi">
+				</a>
 		</div>
 	</div>
 </footer>
