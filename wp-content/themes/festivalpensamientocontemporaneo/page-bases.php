a4482e47306d526f4052c9c97fd4beed98295abd
maxi falcone <maxifalcone@gmail.com>
2019-03-28 13:42:18 -0300

    fix que enviaron los de anfibia

--- wp-content/themes/festivalpensamientocontemporaneo/page-bases.php
+++ wp-content/themes/festivalpensamientocontemporaneo/page-bases.php
@@ -11,16 +11,12 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
-							<div class="col-md-12">
+							<div class="col-md-10 offset-md-2 pl-0">
 								<h1><?php the_title(); ?></h1>
 							</div>
 							<div class="row">
-								<div class="col-md-6">
+								<div class="col-md-8 offset-md-2">
 								<?php the_content(); ?>
-								</div>
-								<div class="col-md-6  pt-5">
-									<img src="<?php bloginfo('template_directory'); ?>/img/fallaras.jpg" alt="Cristina Fallaras" class="float-right border border-dark">
-									<p class="text-light bg-dark float-right p-2 w-100" style="max-width: 402px;">Cristina Fallarás</p>
 								</div>
 							</div>
 				<?php endwhile;?>
