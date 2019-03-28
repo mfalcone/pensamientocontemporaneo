da0bc4f501f2b97fab0ced788c1fba137c291892
Maximiliano Falcone <maximilianofalcone@Maximilianos-Mac-mini.local>
2019-03-28 11:05:06 -0300

    form

--- wp-content/themes/festivalpensamientocontemporaneo/page-formulario.php
+++ wp-content/themes/festivalpensamientocontemporaneo/page-formulario.php
@@ -16,8 +16,8 @@ get_header(); ?>
 								<?php the_content(); ?>
 								</div>
 								<div class="col-md-8">
-									<div class="embed-container" style="height: 1600px">
-										<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkFKV49jrlFGRtRJ13gj9Xg9shhUwZ95jTFxCXmUgRR-HCjQ/viewform?embedded=true" width="640" height="1602" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+									<div class="embed-container" style="height: 2200px">
+										<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkFKV49jrlFGRtRJ13gj9Xg9shhUwZ95jTFxCXmUgRR-HCjQ/viewform?usp=pp_url&entry.307578052=Lugar,+Fecha%0A%0AAtte+Festival+de+Pensamiento+Contempor%C3%A1neo+:%0APor+la+presente+dejo+constancia+de+que+el+medio+%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6.+est%C3%A1+interesado+en+que+el/la+periodista/editor/a+%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6+con+documento+%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6%E2%80%A6.+viaje+a+Rosario,+Argentina+y+participe+en+el+Taller+de+Cr%C3%B3nica+cultural:+Rosario,+una+ciudad+anfibia,+en+el+marco+del+festival+Pensamiento+Contempor%C3%A1neo+que+se+desarrollar%C3%A1+desde+el+13+de+mayo+hasta+el+18+de+mayo+de+2019%0A%0AFirma%0AAclaraci%C3%B3n%0ACargo%0AMedio" width="640" height="1602" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
 									</div>
 								</div>
 							</div>
