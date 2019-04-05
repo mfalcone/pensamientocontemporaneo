e2fa62737f0aec74a2bcf9be3c1403e58e89d2c5
maxi falcone <maxifalcone@gmail.com>
2019-04-05 18:01:16 -0300

    pagina el festival

--- wp-content/themes/festivalpensamientocontemporaneo/page-bases.php
+++ wp-content/themes/festivalpensamientocontemporaneo/page-bases.php
@@ -7,21 +7,20 @@
 */
 get_header(); ?>
 <div class="container content">
-		
-			<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-							<div class="col-md-12 pl-0">
-								<h3 class="nja">Rosario, una ciudad anfibia</h3>
-								<h1><?php the_title(); ?></h1>
-							</div>
-							<div class="row">
-								<?php the_content(); ?>
-							</div>
-				<?php endwhile;?>
+		<?php while ( have_posts() ) : the_post(); ?>
+					<div class="col-md-12 pl-0">
+						<h3 class="nja">Rosario, una ciudad anfibia</h3>
+						<h1><?php the_title(); ?></h1>
+					</div>
+					<div class="row">
+						<?php the_content(); ?>
+					</div>
+		<?php endwhile;?>
 
-			<?php else :?>
-				404
-			<?php endif;?>
-		</div>
+	<?php else :?>
+		404
+	<?php endif;?>
+</div>
 <?php get_footer(); ?>
--- wp-content/themes/festivalpensamientocontemporaneo/page-festival.php
+++ wp-content/themes/festivalpensamientocontemporaneo/page-festival.php
@@ -1,17 +1,23 @@
 <?php
 /**
- * page
- */
+ * 	Template Name: Festival Page
+ *
+ *	
+ *
+*/
 get_header();?>
 <div class="container content">
 	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 					<div class="col-md-12 pl-0">
-						<h1><?php the_title(); ?></h1>
+						<h1>Pensamiento contemporaneo, <br><span class="sec-pr">un festival anfibio</span></h1>
 					</div>
 					<div class="row">
 						<?php the_content(); ?>
+					</div>
+					<div class="row" style="margin-bottom:-100px;margin-top:200px">
+						<img src="<?php echo get_template_directory_uri() ?>/img/img_elfestival.png" alt="el festival"> 
 					</div>
 		<?php endwhile;?>
 
