24dc2d2acb1e0555bc19c9884d2955f2bd2fed4c
maxi falcone <maxifalcone@gmail.com>
2019-05-08 10:59:22 -0300

    mapa

---
+++ wp-content/themes/festivalpensamientocontemporaneo/page-mapa.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 	Template Name: mapa Page
+ *
+ *	
+ *
+*/
+get_header();?>
+
+<div class="container content">
+	<?php if ( have_posts() ) : ?>
+		<div class="row">
+		<?php while ( have_posts() ) : the_post(); ?>
+					<div class="col-md-12 pl-0">
+						<h1><?php the_title(); ?></h1>
+					</div>
+					<div class="col-md-12">
+						<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1dmZPQJARZqqj7NhlNz7Qg7x1Pyj4kdMG" width="100%" height="600"></iframe>
+					</div>
+					<div class="col-md-12 dos-columnas lista-mapas">
+						<?php the_content(); ?>
+					</div>
+		<?php endwhile;?>
+		</div>
+	<?php else :?>
+		404
+	<?php endif;?>
+</div>
+
+<?php get_footer();?>
